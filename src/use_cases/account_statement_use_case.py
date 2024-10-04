93fa6c5fbef0a163bb2186f43871af9183bee3e5
John Paul Ong <jpong5202@gmail.com>
2024-10-04 19:03:07 +0800

    Add generate account statement use case

---
+++ src/use_cases/account_statement_use_case.py
@@ -0,0 +1,17 @@
+from uuid import UUID
+
+from infrastructure.account_repository import AccountRepository
+
+
+class AccountStatementUseCase:
+    def __init__(self, account_repository):
+        self.account_repository: AccountRepository = account_repository
+
+    def generate_account_statement(self, account_id: UUID) -> str:
+        try:
+            account = self.account_repository.find_account_by_id(account_id=account_id)
+            return (
+                f"Account: {account.account_number}: \nBalance: {account.get_balance()}"
+            )
+        except ValueError as e:
+            return str(e)
